8e9e6596bdb483e55e30d5330ecf6b706cc5a385
Anant <anantvijay239@gmail.com>
2025-02-06 19:01:08 +0530

    "Fixed more rules in basic game structure"

--- chess/rules.py
+++ chess/rules.py
@@ -1,18 +1,22 @@
 from chess.board import Board
+from chess.pieces import Rook, Bishop, Queen, King, Knight, Pawn
 
 class Rules:
+    @staticmethod
+    # Inside rules.py
+
     @staticmethod
     def is_check(board, king_position, color):
         """Check if the king of the given color is in check."""
         row, col = king_position
-        # Check all directions for pieces that could attack the king
         for r in range(8):
             for c in range(8):
                 piece = board.board[r][c]
-                if piece and piece.color != color:
-                    if (row, col) in piece.valid_moves((r, c), board.board):
+                if piece and piece.color != color:  # Only consider opponent's pieces
+                    if (row, col) in piece.valid_moves((r, c), board):  # Pass 'board' instead of 'board.board'
                         return True
         return False
+
 
     @staticmethod
     def is_checkmate(board, king_position, color):
@@ -65,45 +69,6 @@ class Rules:
 
         return True  # Move is valid
 
-
-
-    @staticmethod
-    def is_valid_move(start, end, board):
-        """Validate if the move from start to end is legal for the piece at start."""
-        start_row, start_col = start
-        end_row, end_col = end
-        piece = board.board[start_row][start_col]
-        
-        if piece is None:
-            return False  # No piece to move
-        
-        valid_moves = piece.valid_moves((start_row, start_col), board.board)
-        if (end_row, end_col) in valid_moves:
-            target_piece = board.board[end_row][end_col]
-            if target_piece is None or target_piece.color != piece.color:
-                # Simulate the move
-                temp_board = Board()
-                temp_board.board = [row[:] for row in board.board]
-                temp_board.board[end_row][end_col] = temp_board.board[start_row][start_col]
-                temp_board.board[start_row][start_col] = None
-
-                # Check if the move puts the king in check
-                king_position = None
-                for r in range(8):
-                    for c in range(8):
-                        temp_piece = temp_board.board[r][c]
-                        if temp_piece and temp_piece.__class__.__name__.lower() == 'king' and temp_piece.color == piece.color:
-                            king_position = (r, c)
-                            break
-
-                if not king_position:
-                    return False
-
-                if Rules.is_check(temp_board, king_position, piece.color):
-                    return False
-
-                return True
-        return False
     
     
     @staticmethod
@@ -133,4 +98,162 @@ class Rules:
             # Undo the move
             board.board[move[0]][move[1]] = original_piece
         return valid_moves
+    
+    @staticmethod
+    def is_valid_move(start, end, board, color):
+        """Validate if the move from start to end is legal for the piece at start, considering check."""
+        start_row, start_col = start
+        end_row, end_col = end
+        piece = board.board[start_row][start_col]
+        
+        if piece is None:
+            return False  # No piece to move
 
+        # If the king is in check, only moves that alleviate the check should be allowed
+        if Rules.is_king_in_check(board, color):
+            # If the move doesn't either move the king, block the check, or capture the attacking piece, it's invalid
+            if not (Rules.is_checkmate(board, (start_row, start_col), color) or 
+                    piece.__class__.__name__.lower() == 'king' or 
+                    Rules.can_block_or_capture_check(start_row, start_col, piece, end_row, end_col, board)):
+                return False
+
+        valid_moves = piece.valid_moves((start_row, start_col), board.board)
+        if (end_row, end_col) in valid_moves:
+            target_piece = board.board[end_row][end_col]
+            if target_piece is None or target_piece.color != piece.color:
+                # Simulate the move
+                temp_board = Board()
+                temp_board.board = [row[:] for row in board.board]
+                temp_board.board[end_row][end_col] = temp_board.board[start_row][start_col]
+                temp_board.board[start_row][start_col] = None
+
+                # Check if the move puts the king in check
+                king_position = None
+                for r in range(8):
+                    for c in range(8):
+                        temp_piece = temp_board.board[r][c]
+                        if temp_piece and temp_piece.__class__.__name__.lower() == 'king' and temp_piece.color == piece.color:
+                            king_position = (r, c)
+                            break
+
+                if not king_position:
+                    return False
+
+                if Rules.is_check(temp_board, king_position, piece.color):
+                    return False
+
+                return True
+        return False
+    
+
+    @staticmethod
+    def can_block_or_capture_check(start_row, start_col, piece, end_row, end_col, board):
+        """Check if a move can block or capture the checking piece."""
+        # Get the position of the king of the current color
+        king_pos = None
+        for r in range(8):
+            for c in range(8):
+                temp_piece = board.board[r][c]
+                if temp_piece and temp_piece.__class__.__name__.lower() == 'king' and temp_piece.color == piece.color:
+                    king_pos = (r, c)
+                    break
+            if king_pos:
+                break
+
+        # Check if the piece is blocking or capturing the attacker
+        check_row, check_col = Rules.get_checking_piece_position(board, king_pos, piece.color)
+
+        if check_row is None and check_col is None:
+            return False  # No check detected, can't block or capture
+
+        # Check if we can block the attack
+        if piece.__class__.__name__.lower() in ['rook', 'bishop', 'queen']:
+            return Rules.can_block_check_with_line_piece(start_row, start_col, piece, check_row, check_col, board)
+        # Check if we can capture the checking piece
+        if piece.__class__.__name__.lower() == 'king':
+            return Rules.can_capture_checking_piece(start_row, start_col, piece, check_row, check_col, board)
+        
+        return False
+
+    @staticmethod
+    def can_block_check_with_line_piece(start_row, start_col, piece, check_row, check_col, board):
+        """Block the check with a piece that moves along a line (Rook, Bishop, Queen)."""
+        if isinstance(piece, (Rook, Queen)):
+            if start_col == check_col:
+                return Rules.is_between_check_and_king(start_row, start_col, check_row, check_col, board)
+            elif start_row == check_row:
+                return Rules.is_between_check_and_king(start_row, start_col, check_row, check_col, board)
+        elif isinstance(piece, Bishop):
+            if abs(start_row - check_row) == abs(start_col - check_col):
+                return Rules.is_between_check_and_king(start_row, start_col, check_row, check_col, board)
+        return False
+
+    @staticmethod
+    def can_capture_checking_piece(start_row, start_col, piece, check_row, check_col, board):
+        """Check if a piece can capture the piece that is delivering the check."""
+        if isinstance(piece, King):
+            # Kings can only capture a checking piece if it's within one square in any direction
+            if abs(start_row - check_row) <= 1 and abs(start_col - check_col) <= 1:
+                target_piece = board.board[check_row][check_col]
+                if target_piece and target_piece.color != piece.color:
+                    return True
+        return False
+
+    @staticmethod
+    def is_between_check_and_king(start_row, start_col, check_row, check_col, board):
+        """Check if a piece can move between the checking piece and the king."""
+        if start_row == check_row:
+            if start_col > check_col:
+                for c in range(check_col + 1, start_col):
+                    if board.board[start_row][c] is not None:
+                        return False
+            else:
+                for c in range(start_col + 1, check_col):
+                    if board.board[start_row][c] is not None:
+                        return False
+        elif start_col == check_col:
+            if start_row > check_row:
+                for r in range(check_row + 1, start_row):
+                    if board.board[r][start_col] is not None:
+                        return False
+            else:
+                for r in range(start_row + 1, check_row):
+                    if board.board[r][start_col] is not None:
+                        return False
+        elif abs(start_row - check_row) == abs(start_col - check_col):
+            row_step = 1 if check_row > start_row else -1
+            col_step = 1 if check_col > start_col else -1
+            r, c = start_row + row_step, start_col + col_step
+            while (r != check_row) and (c != check_col):
+                if board.board[r][c] is not None:
+                    return False
+                r += row_step
+                c += col_step
+        return True
+
+    @staticmethod
+    def get_checking_piece_position(board, king_pos, color):
+        """Get the position of the piece delivering the check."""
+        check_row, check_col = None, None
+        # Search for the attacking piece on the board
+        for r in range(8):
+            for c in range(8):
+                piece = board.board[r][c]
+                if piece and piece.color != color:
+                    if isinstance(piece, (Rook, Bishop, Queen)):
+                        if Rules.is_attack_possible(piece, r, c, king_pos[0], king_pos[1], board):
+                            check_row, check_col = r, c
+                            break
+                    elif isinstance(piece, Knight):
+                        if Rules.is_knight_check(piece, r, c, king_pos[0], king_pos[1]):
+                            check_row, check_col = r, c
+                            break
+                    elif isinstance(piece, King):
+                        if abs(r - king_pos[0]) <= 1 and abs(c - king_pos[1]) <= 1:
+                            check_row, check_col = r, c
+                            break
+                    elif isinstance(piece, Pawn):
+                        if Rules.is_pawn_check(piece, r, c, king_pos[0], king_pos[1]):
+                            check_row, check_col = r, c
+                            break
+        return check_row, check_col
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from chess.board import Board
 from chess.pieces import Piece, Pawn, Rook, Knight, Bishop, Queen, King
+from chess.rules import Rules  # Import Rules class to handle check/valid moves
 
 # Initialize Pygame
 pygame.init()
@@ -60,24 +61,110 @@ def draw_pieces(win, board, pieces):
                         piece_rect = piece_image.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
                         win.blit(piece_image, piece_rect.topleft)
 
-
 # Highlight valid moves with different colors for regular and capture moves
-def highlight_moves(win, moves, board, selected_piece):
+def highlight_moves(win, moves, board, selected_piece, king_position, turn):
     highlight_surface = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)  # Allow transparency
-    for move in moves:
-        row, col = move
-        target_piece = board.board[row][col]
-        
-        # If target square contains a piece and it's an opponent's piece, it's a capture
-        if target_piece is not None and target_piece.color != selected_piece.color:
-            highlight_surface.fill(CAPTURE_COLOR)  # Red for capture
-        elif target_piece is None:
-            highlight_surface.fill(HIGHLIGHT_COLOR)  # Green for regular moves
-        else:
-            # If target square contains the same color piece, no need to highlight
-            continue
-        
-        win.blit(highlight_surface, (col * SQUARE_SIZE, row * SQUARE_SIZE))
+    attacking_piece = None
+
+    if king_position:
+        # Find the attacking piece (that is checking the king)
+        for row in range(8):
+            for col in range(8):
+                piece = board.board[row][col]
+                if piece and piece.color != turn:  # Opponent's pieces
+                    if (king_position[0], king_position[1]) in piece.valid_moves((row, col), board):
+                        attacking_piece = piece
+                        break
+
+    # If the king is in check, highlight only valid blocking/capture moves
+    if attacking_piece:
+        can_block_or_capture = False
+        for move in moves:
+            row, col = move
+            target_piece = board.board[row][col]
+            
+            # If this move can block or capture the attacking piece, highlight it
+            if target_piece is None or target_piece.color != selected_piece.color:
+                # Implement logic to check if a piece can block the attack or capture the attacking piece
+                if is_blocking_move(move, attacking_piece, king_position, board) or \
+                   is_capturing_move(move, attacking_piece, board):
+                    can_block_or_capture = True
+                    highlight_surface.fill(HIGHLIGHT_COLOR)  # Green for block or capture
+                    win.blit(highlight_surface, (col * SQUARE_SIZE, row * SQUARE_SIZE))
+                else:
+                    continue
+        if not can_block_or_capture:
+            return  # If no valid block/capture moves, return without highlighting
+    else:
+        # Otherwise, highlight all valid moves normally
+        for move in moves:
+            row, col = move
+            target_piece = board.board[row][col]
+            
+            # If target square contains a piece and it's an opponent's piece, it's a capture
+            if target_piece is not None and target_piece.color != selected_piece.color:
+                highlight_surface.fill(CAPTURE_COLOR)  # Red for capture
+            elif target_piece is None:
+                highlight_surface.fill(HIGHLIGHT_COLOR)  # Green for regular moves
+            else:
+                continue
+
+            win.blit(highlight_surface, (col * SQUARE_SIZE, row * SQUARE_SIZE))
+
+def is_blocking_move(move, attacking_piece, king_position, board):
+    """Check if the move can block the check on the king"""
+    # Get the direction of the attacking piece's movement (relative to the king)
+    attacking_row, attacking_col = king_position
+    block_row, block_col = move  # The square that may block the attack
+
+    # Direction vectors for horizontal, vertical, and diagonal directions
+    direction_vectors = {
+        'vertical': [(1, 0), (-1, 0)],
+        'horizontal': [(0, 1), (0, -1)],
+        'diagonal': [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+    }
+
+    # Depending on the type of attacking piece (e.g., rook, bishop, queen), we need to check
+    # if the move is between the attacker and the king.
+
+    if isinstance(attacking_piece, (Rook, Queen)):  # Horizontal or vertical attack
+        if attacking_row == block_row:  # Same row
+            if min(attacking_col, block_col) < block_col < max(attacking_col, block_col):
+                # Check if there is any piece between the attacker and the king in this row
+                for col in range(min(attacking_col, block_col) + 1, max(attacking_col, block_col)):
+                    if board.board[block_row][col] is not None:
+                        return False  # There is a piece in the way, cannot block
+                return True  # No pieces in the way, valid block move
+        elif attacking_col == block_col:  # Same column
+            if min(attacking_row, block_row) < block_row < max(attacking_row, block_row):
+                # Check if there is any piece between the attacker and the king in this column
+                for row in range(min(attacking_row, block_row) + 1, max(attacking_row, block_row)):
+                    if board.board[row][block_col] is not None:
+                        return False  # There is a piece in the way, cannot block
+                return True  # No pieces in the way, valid block move
+
+    elif isinstance(attacking_piece, (Bishop, Queen)):  # Diagonal attack
+        if abs(attacking_row - block_row) == abs(attacking_col - block_col):
+            row_step = 1 if block_row > attacking_row else -1
+            col_step = 1 if block_col > attacking_col else -1
+            # Check if there is any piece between the attacker and the king along the diagonal
+            curr_row, curr_col = attacking_row + row_step, attacking_col + col_step
+            while (curr_row, curr_col) != (block_row, block_col):
+                if board.board[curr_row][curr_col] is not None:
+                    return False  # There is a piece in the way, cannot block
+                curr_row += row_step
+                curr_col += col_step
+            return True  # No pieces in the way, valid block move
+    return False  # If none of the conditions match, it's not a valid block
+
+
+def is_capturing_move(move, attacking_piece, board):
+    """Check if the move can capture the attacking piece"""
+    row, col = move
+    target_piece = board.board[row][col]
+    if target_piece and target_piece == attacking_piece:
+        return True
+    return False
 
 # Reset the board to the initial setup
 def reset_board(board):
@@ -90,6 +177,15 @@ def reset_board(board):
 def is_game_over(board):
     # Implement game over logic here
     return False
+
+def find_king(board, color):
+    for row in range(8):
+        for col in range(8):
+            piece = board.board[row][col]
+            if isinstance(piece, King) and piece.color == color:
+                return row, col
+    return None  # King not found (should never happen if the board is set up correctly)
+
 
 # Main game loop
 def main():
@@ -106,6 +202,9 @@ def main():
     selected_pos = None
     turn = 'white'
 
+    # Create an instance of Rules to check for check
+    rules = Rules()
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -115,13 +214,30 @@ def main():
                 pos = pygame.mouse.get_pos()
                 row, col = pos[1] // SQUARE_SIZE, pos[0] // SQUARE_SIZE
                 if selected_piece:
+                    # Check if the move is valid and king is not in check after move
                     valid_moves = selected_piece.valid_moves(selected_pos, board)
                     if (row, col) in valid_moves:
+                        # Apply the move
                         board.board[selected_pos[0]][selected_pos[1]] = None
                         board.board[row][col] = selected_piece
-                        turn = 'black' if turn == 'white' else 'white'
-                    selected_piece = None
-                    selected_pos = None
+                        
+                        # Check if the move results in the king being in check
+                        king_pos = find_king(board, selected_piece.color)
+                        if king_pos:
+                            if rules.is_check(board,king_pos,selected_piece.color):
+                                # Undo the move if king is in check
+                                board.board[row][col] = None
+                                board.board[selected_pos[0]][selected_pos[1]] = selected_piece
+                                print("Move would result in check! Try again.")
+                            else:
+                                # Switch turns
+                                turn = 'black' if turn == 'white' else 'white'
+                        
+                            selected_piece = None
+                            selected_pos = None  # Reset selected piece and position after a move
+                    else:
+                        selected_piece = None
+                        selected_pos = None  # Reset if the move is not valid, deselect the piece
                 else:
                     piece = board.board[row][col]
                     if piece and piece.color == turn:
@@ -133,8 +249,8 @@ def main():
 
         draw_board(win)
         if selected_piece:
-            valid_moves = selected_piece.valid_moves(selected_pos , board)
-            highlight_moves(win, valid_moves, board, selected_piece)
+            valid_moves = selected_piece.valid_moves(selected_pos, board)
+            highlight_moves(win, valid_moves, board, selected_piece, find_king(board, selected_piece.color), turn)
         draw_pieces(win, board, pieces)
         if is_game_over(board):
             print("Game Over")
